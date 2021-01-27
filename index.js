678eda88836678ab7a5d3809f0e7ca7067c8b0aa
pilkyoo <74221605+pilkyoo@users.noreply.github.com>
2021-01-27 18:52:16 +0900

    second

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hi");
+import express from "express";
+
+var app = express();
+
+const handleListen = ()=>{
+    console.log(`listening -- on : localhost:4000`);
+}
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world!!!');
+});
+
+app.listen(4000, handleListen);
