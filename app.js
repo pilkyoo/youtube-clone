6dc575b8b33df290a3336ca2e97dfcd7129f9227
pilkyoo <74221605+pilkyoo@users.noreply.github.com>
2021-01-29 21:42:58 +0900

    2.13 PUG
    first try to add pug
    so you can see how to add pug.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import routes from "./routes";
 
 var app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -17,8 +18,8 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 
-app.use("/", globalRouter);	app.use(routes.home, globalRouter);
-app.use("/user", userRouter);	app.use(routes.users, userRouter);
-app.use("/video", videoRouter);	app.use(routes.videos, videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
