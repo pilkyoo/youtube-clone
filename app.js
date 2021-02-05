932ff57cad0c7734331059db809a46a30f750703
pilkyoo <74221605+pilkyoo@users.noreply.github.com>
2021-02-05 21:18:04 +0900

    2.16 Local Variables in Pug (globally use)
    time to add information to templete.
    controller has information.
    and templete have to get this information.
    we can know how to send and get information

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ import userRouter  from "./routers/userRouter";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
+import { localMiddlewares } from "./middlewares";
 
 var app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
 app.use(morgan("dev"));
-
+app.use(localMiddlewares);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
